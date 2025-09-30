c3da476756779f1395f26c5d17e678ac3655667b
Era-smus <erasmussaat1@gmail.com>
2025-09-30 23:22:54 +0300

    Modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(resp => resp.json())
+  .then(obj => {
+    // Append the new id to the DOM
+    document.body.innerHTML = obj.id;
+  })
+  .catch(error => {
+    // Append the error message to the DOM
+    document.body.innerHTML = error.message;
+  });
+}
+
+// expose globally so tests can see it
+window.submitData = submitData;
